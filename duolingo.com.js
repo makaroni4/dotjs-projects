d961aeae3dd865dbc5775785d14002103ae4d463
Anatoli Makarevich <makaroni4@gmail.com>
2015-09-28 21:39:59 +0200

    Add implementation with Mutation Observers

--- duolingo.com.js
+++ duolingo.com.js
@@ -1,35 +1,38 @@
-$(function(){
+(function() {
   function drawLessonsCount(count) {
-    $(".topbar-nav-main").append("<li style='display: block; color: #fff; padding: 8px 15px;'>Осталось уроков: " + count + "</li>");
-  }
-
-  function countLessons() {
-    var count = 0;
-
-    $(".lessons-left").each(function(index, element) {
-      var text = $(element).text();
-
-      if(text) {
-        count += text.split("/")[1] - text.split("/")[0];
-      }
-    })
-
-    return count;
-  }
-
-  function timerHandler() {
-    console.log(".JS >>> Trying to find number of lessons");
-
-    if($(".skill-tree").length) {
-      var numberOfLessons = countLessons();
-      drawLessonsCount(numberOfLessons);
-      stopTimer();
+    if($(".dot-js-lessons-count").length) {
+      $(".dot-js-lessons-count").text(count);
+    } else {
+      $(".topbar-nav-main").append("<li style='display: block; color: #fff; padding: 8px 15px;'>Осталось уроков: <span class='dot-js-lessons-count'>" + count + "</span></li>");
     }
   }
 
-  function stopTimer() {
-    clearInterval(timer);
-  }
+  var MutationObserver = window.MutationObserver;
+  var lessonsLeft = 0;
 
-  var timer = setInterval(function(){ timerHandler() }, 1000);
-});
+  var observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+      var newNodes = mutation.addedNodes;
+      var $nodes = $(newNodes);
+
+      $nodes.each(function() {
+        $(this).find(".lessons-left:visible").each(function() {
+          var rawCounts = $(this).text();
+
+          if(rawCounts) {
+            var lessonsInChapter = rawCounts.split("/")[1];
+            var tookLessonsInChapter = rawCounts.split("/")[0];
+
+            lessonsLeft += lessonsInChapter - tookLessonsInChapter;
+          }
+        });
+
+        drawLessonsCount(lessonsLeft);
+      });
+    });
+  });
+
+  observer.observe(document.body, {
+    childList: true
+  });
+})();
